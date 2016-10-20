591666f3a38ac9d53968de6740a3b72a1f5f0ec9
Ruhul Amin <r_amin105@yahoo.com>
2016-10-20 15:16:34 +0200

    final Version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,147 @@
 var map;
 var markers = [];
-//locations array with first 5 locations in forest hills
+//restaurants array with first 5 restaurants in Berlin
 var Model = {
     restaurants : [
         {
             name: "Lovely Sushi",
             position: {lat: 52.529461, lng: 13.472718},
-            description: "Sushi Restaurant"
+            description: "Sushi Restaurant",
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/EZ04y3oDuhWk6Q_crHiqpw/ms.jpg',
+            rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png'
         }, {
             name: "Allee Bistro",
             position: {lat: 52.535247, lng: 13.496363},
-            description: "Sushi Restaurant"
+            description: "Sushi Restaurant",
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/EZ04y3oDuhWk6Q_crHiqpw/ms.jpg',
+            rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png'
         }, {
             name: "Reisschale",
             position: {lat:52.516251, lng: 13.481724},
-            description: "Vietnamese Restaurant "
+            description: "Vietnamese Restaurant ",
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/EZ04y3oDuhWk6Q_crHiqpw/ms.jpg',
+            rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png'
         }, {
             name: "Chum & Friends",
             position: {lat: 52.513532, lng: 13.458728},
-            description: "Pan-Asian Restaurant"
+            description: "Pan-Asian Restaurant",
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/EZ04y3oDuhWk6Q_crHiqpw/ms.jpg',
+            rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png'
         }, {
             name: "Zhou´s Five",
             position: {lat: 52.504157, lng: 13.473256},
-            description: "Asian Restaurant"
+            description: "Asian Restaurant",
+            image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/EZ04y3oDuhWk6Q_crHiqpw/ms.jpg',
+            rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png'
         }
     ],
-//custom styling of map courtesy of snazzy maps user Simon Goellner
-    //styles: [{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#747474"},{"lightness":"23"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"color":"#f38eb0"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#ced7db"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#ffa5a8"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#c7e5c8"}]},{"featureType":"poi.place_of_worship","elementType":"geometry.fill","stylers":[{"color":"#d6cbc7"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#c4c9e8"}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"color":"#b1eaf1"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":"100"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"},{"lightness":"100"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffd4a5"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffe9d2"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"weight":"3.00"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"weight":"0.30"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#747474"},{"lightness":"36"}]},{"featureType":"road.local","elementType":"labels.text.stroke","stylers":[{"color":"#e9e5dc"},{"lightness":"30"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":"100"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#d2e7f7"}]}]
+    yelprestaurants: [] // restaurants will be stored from YelpAPI
 };
 
+/**
+ * @description Represent Yelp data
+ * @constructor
+ */
+var generateYelpContentString = function () {
+    var consumerKey = "DBs4_OjE5q5fKwa7Ij2Wmw";
+    var consumerSecret = "yqtTsYBRlvkJ9HDASuzgV5wDKIw";
+    var accessToken = "4y_AIdBDaVTfS0sgLAv4rfEbQrk0hetg";
+    var accessTokenSecret = "C3McKT9qiuL0s-L_u8rX5OyBX84";
+
+    // to generate random nonce which required by Yelp
+    function nonce_generate() {
+        return (Math.floor(Math.random() * 1e12).toString());
+    }
+
+    var yelp_url = "http://api.yelp.com/v2/search/";
+
+    var parameters = {
+        oauth_consumer_key: consumerKey,
+        oauth_token: accessToken,
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now() / 1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0',
+        callback: 'cb',
+        term: 'food',
+        location: 'Berlin,DE-BE',
+        sort: 2,
+        limit: 20
+
+    };
+
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, consumerSecret, accessTokenSecret);
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+        url: yelp_url,
+        data: parameters,
+        cache: true,
+        dataType: 'jsonp'
+    };
+
+    return settings;
+};
+
+/**
+ * @description Ajax call to get data from yelp and create restaurants data
+ * @constructor
+ * @param function generateYelpContentString - passed a function as parameter
+ * @param Object - return restaurants object on success or through as error
+ */
+$.ajax(generateYelpContentString())
+    .done(function (results) {
+        var center = results.region.center;
+        createRestaurants(results.businesses);  //  Call a function to generate restaurants data
+        initMap(center);  // initiate Google map
+    })
+    .fail(function () {
+        alert("Error loading data from Yelp");
+    });
+
+var createRestaurants = function (yelpRestaurants) {
+    // To append yelp restaurants data into data model
+    yelpRestaurants.forEach(function (data) {
+        var restaurantItem = {
+            name : data.name,
+            position: {lat: data.location.coordinate.latitude, lng: data.location.coordinate.longitude},
+            description: data.snippet_text,
+            image_url: data.image_url,
+            rating_img_url: data.rating_img_url
+        }
+        Model.yelprestaurants.push(restaurantItem);
+    });
+};
+
+// Single restaurant model
 var Restaurant = function (data) {
     this.name = ko.observable(data.name);
     this.position = ko.observable(data.position);
-    this.description = ko.observable(data.position);
+    this.description = ko.observable(data.description);
+    this.image_url = ko.observable(data.image_url);
+    this.rating_img_url = ko.observable(data.rating_img_url);
 }
 //knockout ViewModel
 var ViewModel = function(){
     var self = this;
     var searchResult;
     var marker;
-    var infowindow;
-
 
     self.search_text = ko.observable('');
-    self.restaurantList = ko.observableArray([]);
+    self.restaurantList = ko.observableArray();
     self.showFilteredMarkers = ko.observable(); // restaurant to store the filter
 
     //iterates through locations in Model and adds info to markers
-    Model.restaurants.forEach(function (restaurantItem) {
+   Model.restaurants.forEach(function (restaurantItem) {
         self.restaurantList.push( new Restaurant(restaurantItem));
     });
 
-    // Filter
+    //Data from Model and adds into restaurantList
+    Model.yelprestaurants.forEach(function (restaurantItem) {
+        self.restaurantList.push( new Restaurant(restaurantItem));
+    });
+
+    // Filter based on user text
     self.filterRestaurants = ko.computed(function () {
         if (!self.search_text()) {
             searchResult = self.restaurantList();
@@ -62,60 +152,129 @@ var ViewModel = function(){
                 )
             });
         }
+
+        // Call showFilteredMarkers to visible only those markers, matched from user input
         self.showFilteredMarkers(searchResult, self.restaurantList());
         return searchResult;
     });
 
+    // To make visible user serach result only
     self.showFilteredMarkers = function(filteredSearchArray, restaurantsArray) {
 
         for (var i = 0; i < restaurantsArray.length; i++) {
-            //restaurantsArray[i].marker.setVisible(false);
+            restaurantsArray[i].marker.setVisible(false);
         }
 
         for (var i = 0; i < filteredSearchArray.length; i++) {
 
-            filteredSearchArray[i].marker.setVisible(true);
+            restaurantsArray[i].marker.setVisible(true);
         }
 
     };
 
+
+    // To Generate marker and it's  other properties
     for(var i = 0; i < self.restaurantList().length; i++){
 
         marker = new google.maps.Marker({
             map: map,
             position: self.restaurantList()[i].position(),
             title: self.restaurantList()[i].name(),
-            //content: Model.locations[i].comment,
             animation: google.maps.Animation.DROP,
             icon: 'assets/img/map-marker.png'
         });
-        //adding content to infowindow
-        infowindow = new google.maps.InfoWindow({
-            title: self.restaurantList()[i].name(),
-            content: self.restaurantList()[i].description()
-        });
-        //added click function to toggle Animation
-        //color and infowindow props of clicked marker
-        marker.addListener('click', function(){
-            if (this.getAnimation() !== null) {
-                this.setAnimation(null);
-                this.setIcon('assets/img/map-marker.png');
-                infowindow.close(map, this);
-            } else {
-                this.setAnimation(google.maps.Animation.BOUNCE);
-                this.setIcon('assets/img/map-marker.png');
-                infowindow.open(map, this);
-            }
-        })
-        //pushes all premade marker from for loop to markers array defined in //line 2
+        self.restaurantList()[i].marker = marker;
+
+        // InfoWindow content
+        var popupContent = '<div id="iw-container">' +
+            '<div class="iw-title">' + self.restaurantList()[i].name() +'</div>' +
+            '<div class="iw-content">' +
+            '<div class="iw-subTitle">Description</div>' +
+            '<img src='+ self.restaurantList()[i].image_url() +' alt="' + self.restaurantList()[i].name() +'" height="115" width="83">' +
+            '<p>' +self.restaurantList()[i].description()+ '</p>' +
+            '<div class="iw-subTitle">Review</div>' +
+            '<img class="review" src='+ self.restaurantList()[i].rating_img_url() +' alt="Rating">' +
+            '</div>' +
+            '<div class="iw-bottom-gradient"></div>' +
+            '</div>';
+
+        // Call infoWindowHandler to manage popup content
+        infoWindowHandler(marker, popupContent);
+
+        //pushes all premade marker from for loop to markers array defined earlier
         markers.push(marker);
     }
-}
 
+    // generate blank info object
+    var infoWindow = new google.maps.InfoWindow();
+
+    /**
+     * @description Manupulate popup content
+     * @constructor
+     * @param {object} marker - map marker
+     * @param {string} popupContent - popup content
+     */
+    function infoWindowHandler(marker, popupContent) {
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.setContent(popupContent);
+
+            // Reference to the DIV that wraps the bottom of infowindow
+            var iwOuter = $('.gm-style-iw');
+
+            /* Since this div is in a position prior to .gm-div style-iw.
+             * We use jQuery and create a iwBackground variable,
+             * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+             */
+            var iwBackground = iwOuter.prev();
+
+            // Removes background shadow DIV
+            iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+            // Removes white background DIV
+            iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+            // Moves the infowindow 115px to the right.
+            iwOuter.parent().parent().css({left: '115px'});
+
+            // Moves the shadow of the arrow 76px to the left margin.
+            iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+            // Moves the arrow 76px to the left margin.
+            iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+            // Changes the desired tail shadow color.
+            iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+            // Reference to the div that groups the close button elements.
+            var iwCloseBtn = iwOuter.next();
+
+            // Apply the desired effect to the close button
+            iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
+
+            // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+            if($('.iw-content').height() < 140){
+                $('.iw-bottom-gradient').css({display: 'none'});
+            }
+
+            if (marker.getAnimation() !== null) {
+
+                this.setAnimation(null);
+                marker.setIcon('assets/img/map-marker.png');
+                infoWindow.close(map, this);
+            } else {
+                //setTimeout(function(){ this.setAnimation(google.maps.Animation.BOUNCE); }, 750);
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){ marker.setAnimation(null); }, 1050);
+                marker.setIcon('assets/img/map-marker.png');
+                infoWindow.open(map, this);
+            }
+        });
+    }
+}
 //init funtion for google maps API to call map and place on document
-function initMap() {
+function initMap(center) {
     map = new google.maps.Map(document.getElementById('mapcanvas'), {
-        center: {lat: 52.516251, lng: 13.481724},
+        center: {lat: center.latitude, lng: center.longitude},
         zoom: 12
     });
     //ensures ViewModel only runs when google maps call returns successful
